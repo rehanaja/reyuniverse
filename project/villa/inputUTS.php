c963b1df1c39fa197dd2b0f9c6c69527c61350e0
rehanaja <muhamadraihan1611@gmail.com>
2025-06-02 16:40:55 +0700

    Tambah folder project

---
+++ project/villa/inputUTS.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Villa Green Bojong Gede</title>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <th colspan="2">
+                <h1>villa green bojong gede</h1>
+            </th>
+        </tr>
+        <tr>
+            <td>
+                
+            </td>
+            <td></td>
+        </tr>
+    </table>
+</body>
+</html>
